e11ca73116a31a29bfe67c986f12a81deed1b5d1
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-02-10 20:27:01 +0530

    Create pattern2.cpp

---
+++ basic_of_programming/pattern2.cpp
@@ -0,0 +1,15 @@
+//nados
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv){
+    int n;
+    cin >> n;
+   for(int i=1;i<=n;i++){
+       for(int j=1;j<=n-i+1;j++){
+            cout<<"*\t";
+       }
+       cout<<endl;
+   }
+    
+}
